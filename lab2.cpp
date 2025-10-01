ee5700771213f9eecbc9896a13438361d08ecc5d
Nelomi009 <neldori@yandex.ru>
2025-10-01 19:58:26 +0700

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -10,31 +10,38 @@
 using namespace std;
 
 int main() {
-  int temperature;
+  double temperature;
   double pressure;
     
   temperature = 600;
-    
+
+  //от 600 до 900 мы прибовляем по  услвию 100 по задачи 
   while (temperature <= 900 ) {
-    pressure = pow(10, ((-8920.0 / (temperature + 273)) + 7.54));
+    pressure = pow(10.0, ((-8920.0 / (temperature + 273)) + 7.54));
         
-    cout<<fixed; cout.precision(3);
-    cout<<"T= " <<temperature;
-    cout<<"\t\P= " <<pressure <<endl; 
-        
+    cout << fixed; cout.precision(3);
+    cout << "T= " << temperature;
+    cout << "\t\P= " << pressure <<endl; 
+
+    //от 600 до 900 мы прибовляем по  услвию 100 по задачи 
     temperature += 100;
   }
 
   do { 
-    pressure=pow(10, ((-8920.0 / (temperature + 273)) + 7.54));
+    pressure = pow(10, ((-8920.0 / (temperature + 273)) + 7.54));
         
-    cout<<fixed; cout.precision(3);
-    cout<<"T= " <<temperature;
-    cout<<"\t\P = " <<pressure<<endl;
-        
+    cout << fixed;
+    //в ответе нужно 3 знака после запятой
+    cout.precision(3);
+    cout << "T= " << temperature;
+    cout <<"\t\P = " << pressure<<endl;
+
+    //от 1000 до 1100  мы прибовляем по 50  услвию по задачи 
     temperature += 50;
-        
+
+    //от 1000 до 1100  мы прибовляем по  услвию 50 по задачи 
     } while (temperature <= 1100);
     
   return 0;
 }
+
